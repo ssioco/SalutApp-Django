430f8dc6899ede4dc69780a464960468ab8cd45d
ssioco <juanjo.9912@gmail.com>
2022-08-08 14:23:46 -0500

    V1.2

--- Modelos/admin.py
+++ Modelos/admin.py
@@ -1,27 +1,108 @@
 from django.contrib import admin
 from .models import Meta, Tipo_usuario, User, Formulario, Permiso, Tip, User_Formulario, Compra, Plan, Plan_Compra, Herramienta, Metodo_pago, Compra_MetodoPago, Herramienta_User, User_Plan, Factura, Encuesta, User_Encuesta, Tipo_usuario_permiso, Avance, Usuarios_Tip
 # Register your models here.
-admin.site.register(Meta)
-admin.site.register(Tipo_usuario)
-admin.site.register(User)
-admin.site.register(Formulario)
-admin.site.register(Permiso)
-admin.site.register(Tip)
-admin.site.register(User_Formulario)
-admin.site.register(Compra)
-admin.site.register(Plan)
-admin.site.register(Plan_Compra)
-admin.site.register(Herramienta)
-admin.site.register(Metodo_pago)
-admin.site.register(Compra_MetodoPago)
-admin.site.register(Herramienta_User)
-admin.site.register(User_Plan)
-admin.site.register(Factura)
-admin.site.register(Encuesta)
-admin.site.register(User_Encuesta)
-admin.site.register(Tipo_usuario_permiso)
-admin.site.register(Avance)
-admin.site.register(Usuarios_Tip)
+
+@admin.register(Meta)
+class MetaAdmin(admin.ModelAdmin):
+    list_display = ('id','descripcion', 'nombre', )
+    search_fields = ['nombre','id',]
+
+@admin.register(Tipo_usuario)
+class Tipo_usuarioAdmin(admin.ModelAdmin):
+    list_display = ('tipo', )
+    search_fields = ['tipo']
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('cedula', 'nombre', 'apellido', 'correo', 'contrasena', 'meta', 'tipo_usuario', 'fecha_nacimiento', 'calculateAge' )
+    search_fields = ['cedula', 'correo',]
+
+@admin.register(Formulario)
+class FormularioAdmin(admin.ModelAdmin):
+    list_display = ('descripcion', 'tipo', 'fecha', )
+    search_fields = ['fecha',]
+
+@admin.register(Permiso)
+class PermisoAdmin(admin.ModelAdmin):
+    list_display = ('tipo', )
+    search_fields = ['tipo',]
+
+@admin.register(Tip)
+class TipAdmin(admin.ModelAdmin):
+    list_display = ('descripcion', )
+
+@admin.register(User_Formulario)
+class User_FormularioAdmin(admin.ModelAdmin):
+    list_display = ('cedula', 'formulario', )
+    search_fields = [str]
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ('info',)
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('info', 'valor', 'nombre', )
+    search_fields = ['nombre',]
+
+@admin.register(Plan_Compra)
+class Plan_CompraAdmin(admin.ModelAdmin):
+    list_display = ('compra', 'plan', )
+    search_fields = ['compra', 'plan']
+
+@admin.register(Herramienta)
+class HerramientAdmin(admin.ModelAdmin):
+    list_display = ('descripcion', 'nombre', )
+    search_fields = ['nombre',]
+
+@admin.register(Metodo_pago)
+class Metodo_pagoAdmin(admin.ModelAdmin):
+    list_display = ('descripcion',)
+    
+@admin.register(Compra_MetodoPago)
+class Compra_MetodoPagoAdmin(admin.ModelAdmin):
+    list_display = ('metodo_pago', 'compra', )
+    search_fields = ['metodo_pago', 'compra', ]
+
+@admin.register(Herramienta_User)
+class Herramienta_UserAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'herramienta', )
+    search_fields = ['usuario', 'herramienta',]
+
+@admin.register(User_Plan)
+class User_PlanAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'plan', )
+    search_fields = ['usuario', 'plan', ]
+
+@admin.register(Factura)
+class FacturaAdmin(admin.ModelAdmin):
+    list_display = ('fecha', 'info', 'compra', 'metodo_pago', )
+    search_fields = ['fecha', 'compra',]
+
+@admin.register(Encuesta)
+class EncuestaAdmin(admin.ModelAdmin):
+    list_display = ('descripcion',)
+
+@admin.register(User_Encuesta)
+class User_EncuestaAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'encuesta', )
+    search_fields = ['usuario',]
+
+@admin.register(Tipo_usuario_permiso)
+class Tipo_usuario_permisoAdmin(admin.ModelAdmin):
+    list_display = ('permiso', 'tipo_usuario', )
+    search_fields = ['permiso', 'tipo_usuario',]
+
+@admin.register(Avance)
+class AvanceAdmin(admin.ModelAdmin):
+    list_display = ('descripcion', 'usuario', )
+    search_fields = ['usuario',]
+
+@admin.register(Usuarios_Tip)
+class Usuarios_TipAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'tip', )
+    search_fields = ['usuario']
+
 
 
 
--- Modelos/models.py
+++ Modelos/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Meta(models.Model):
     descripcion = models.CharField(max_length = 500)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.descripcion}"
 
 class Tipo_usuario(models.Model):
-    Tipo = models.CharField(max_length = 100)
+    tipo = models.CharField(max_length = 100)
+
+    def __str__(self) -> str:
+        return f"{self.tipo}"
 
 class User(models.Model): #Herencia de el framework
     cedula = models.IntegerField(primary_key = True)
@@ -15,53 +23,96 @@ class User(models.Model): #Herencia de el framework
     contrasena = models.CharField(max_length = 100)
     meta = models.ForeignKey(Meta, on_delete = models.DO_NOTHING)
     tipo_usuario = models.ForeignKey(Tipo_usuario, on_delete = models.DO_NOTHING)
+    fecha_nacimiento = models.DateField()
 
     def __str__(self) -> str:
-        return f"{self.nombre} {self.apellido}"
+        return f"{self.nombre} {self.apellido} {self.correo}"
+
+    def calculateAge(self): 
+        today = date.today() 
+        return today.year - self.fecha_nacimiento.year - ((today.month, today.day) < (self.fecha_nacimiento.month, self.fecha_nacimiento.day))
 
 class Formulario(models.Model):
     descripcion = models.CharField(max_length = 500)
     tipo = models.CharField(max_length = 100)
     fecha = models.DateTimeField(auto_now = True)
 
+    def __str__(self) -> str:
+        return f"{self.tipo} {self.descripcion}"
+
 class Permiso(models.Model):
     tipo =  models.CharField(max_length = 100)
 
+    def __str__(self) -> str:
+        return f"{self.tipo}"
+
 class Tip(models.Model):
     descripcion = models.CharField(max_length = 500)
+
+    def __str__(self) -> str:
+        return f"{self.descripcion}"
 
 class User_Formulario (models.Model):
     cedula = models.ForeignKey(User, on_delete = models.DO_NOTHING)
     formulario = models.ForeignKey(Formulario, on_delete = models.DO_NOTHING)
 
+    def __str__(self) -> str:
+        return f"{self.cedula} {self.formulario}"
+
 class Compra(models.Model):
     info = models.CharField(max_length = 200)
+
+    def __str__(self) -> str:
+        return f"{self.info}"
 
 class Plan(models.Model):
     info = models.CharField(max_length = 200)
     valor = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.valor}"
 
 class Plan_Compra(models.Model):
     compra = models.ForeignKey(Compra, on_delete = models.DO_NOTHING)
     plan = models.ForeignKey(Plan, on_delete = models.DO_NOTHING)
 
+    def __str__(self) -> str:
+        return f"{self.plan} {self.compra}"
+
 class Herramienta(models.Model):
     descripcion = models.CharField(max_length = 200)
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.nombre}"
 
 class Metodo_pago(models.Model):
     descripcion = models.CharField(max_length = 100)
+
+    def __str__(self) -> str:
+        return f"{self.descripcion}"
 
 class Compra_MetodoPago(models.Model):
     metodo_pago = models.ForeignKey(Metodo_pago, on_delete = models.DO_NOTHING)
     compra = models.ForeignKey(Compra, on_delete = models.DO_NOTHING)
 
+    def __str__(self) -> str:
+        return f"{self.metodo_pago} {self.compra}"
+
 class Herramienta_User(models.Model):
     usuario = models.ForeignKey(User, on_delete = models.DO_NOTHING)
     herramienta = models.ForeignKey(Herramienta, on_delete = models.DO_NOTHING)
 
+    def __str__(self) -> str:
+        return f"{self.usuario} {self.herramienta}"
+
 class User_Plan(models.Model):
     usuario = models.ForeignKey(User, on_delete = models.DO_NOTHING)
     plan = models.ForeignKey(Plan, on_delete = models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return f"{self.usuario} {self.plan}"
 
 class Factura(models.Model):
     fecha = models.DateTimeField(auto_now = True)
@@ -69,24 +120,42 @@ class Factura(models.Model):
     compra = models.ForeignKey(Compra, on_delete = models.DO_NOTHING)
     metodo_pago = models.ForeignKey(Metodo_pago, on_delete = models.DO_NOTHING)
 
+    def __str__(self) -> str:
+        return f"{self.fecha} {self.compra} {self.info}"
+
 class Encuesta(models.Model):
     descripcion = models.CharField(max_length=800)
+
+    def __str__(self) -> str:
+        return f"{self.descripcion}"
 
 class User_Encuesta(models.Model):
     usuario = models.ForeignKey(User, on_delete = models.DO_NOTHING)
     encuesta = models.ForeignKey(Encuesta, on_delete = models.DO_NOTHING)
 
+    def __str__(self) -> str:
+        return f"{self.usuario} {self.encuesta}"
+
 class Tipo_usuario_permiso(models.Model):
     permiso = models.ForeignKey(Permiso, on_delete = models.DO_NOTHING)
     tipo_usuario = models.ForeignKey(Tipo_usuario, on_delete = models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return f"{self.permiso} {self.tipo_usuario}"
 
 class Avance(models.Model):
     descripcion = models.CharField(max_length = 250)
     usuario = models.ForeignKey(User, on_delete = models.DO_NOTHING)
 
+    def __str__(self) -> str:
+        return f"{self.usuario} {self.descripcion}"
+
 class Usuarios_Tip(models.Model):
     usuario = models.ForeignKey(User, on_delete = models.DO_NOTHING)
     tip = models.ForeignKey(Tip, on_delete = models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return f"{self.tip} {self.usuario}"
 
 
 
